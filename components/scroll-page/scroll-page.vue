0fcab7ce17c3a1d0cc928614157135215a0abb15
liujiajun <718292306@qq.com>
2024-05-18 22:57:08 +0800

    完成scroll-page组件

--- components/scroll-page/scroll-page.vue
+++ components/scroll-page/scroll-page.vue
@@ -1,17 +1,48 @@
 <template>
-	<scroll-view scroll-y="true" :style = "{height:windowHeight + 'px'}">
-    <view class="scroll-page-content">
-      <slot></slot>
-    </view>
+	<scroll-view 
+		scroll-y="true" 
+		:refresher-enabled = "scrollPageProps.refresherEnabled",
+		:refresher-triggered = "scrollPageProps.refresherTriggered"
+		@refresherrefresh="$emit('refresherrefresh',$event)"
+		@scrolltolower="$emit('scrolltolower',$event)"
+		:style = "{
+			height:windowHeight + 'px',
+			boxSizing: 'border-box',
+			borderBottom: scrollPageProps.borderStyle,
+			}">
+		<view class="scroll-page-content">
+		  <slot></slot>
+		</view>
 	</scroll-view>
 </template>
 
 <script setup>
+	//读取页面视口高度
 	const {windowHeight} = uni.getSystemInfoSync()
+	
+	//自定义组件属性
+	const scrollPageProps = defineProps({
+		borderStyle: {
+			type: [String,Boolean],
+			default: false,
+		},
+		refresherEnabled: {
+		  type: Boolean,
+		  default: false,
+		},
+		refresherTriggered: {
+		  type: Boolean,
+		  default: false,
+		},
+	})
+	
+	//自定义事件
+	defineEmits(['refresherrefresh','scrolltolower'])
 </script>
 
 <style lang="scss">
   .scroll-page-content {
+	height: 1000px;
     padding-bottom: env(safe-area-inset-bottom);
   }
 </style>
--- pages/my/index.vue
+++ pages/my/index.vue
@@ -1,5 +1,10 @@
 <template>
-	<scroll-page></scroll-page>
+	<scroll-page 
+	borderStyle = "1px solid blue"
+	:refresherEnabled = "true",
+	>
+		
+	</scroll-page>
 </template>
 
 <script>
